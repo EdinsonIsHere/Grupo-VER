d295d970a5a673a322380e720e901faa6eea9d37
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 18:30:52 -0500

    Borrar contenido de prueba
    Se borro contenido de prueba de github, que no pertenece al proyecto

--- ColegioHogwarts/src/FuncionesCH/DemoCH.java
+++ ColegioHogwarts/src/FuncionesCH/DemoCH.java
@@ -4,12 +4,6 @@ package FuncionesCH;
 public class DemoCH {
     public static void main(String[] args) {
         System.out.println("Bienvenido al programa del Colegio Hogwarts");
-        System.out.println("Comienza el juego");
-        System.out.println("Y ahora");
-        System.out.println("Ya está, ya entendi esto");
-        System.out.println("Vuelvo a cambiar");
-        
-        System.out.println("Todo se puede en esta vida");
         
     }
     
