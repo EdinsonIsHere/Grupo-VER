a2e5bfe49764bfe39ed578c45dc40472536b4bce
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 14:14:27 -0500

    Creación del programa en netbeans
    Se crea la clase principal DemoCH,
    En esta clase se empezaráa correr nuestra aplicación con un mensaje de bienvenida.

---
+++ ColegioHogwarts/src/FuncionesCH/DemoCH.java
@@ -0,0 +1,9 @@
+
+package FuncionesCH;
+
+public class DemoCH {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al programa del Colegio Hogwarts");
+    }
+    
+}
