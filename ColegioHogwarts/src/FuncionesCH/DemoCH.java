e27a04ab027744b7676a04b06d0092a45dbaa69c
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 15:04:04 -0500

    Mensajes de prueba
    Se imprime en pantalla mensajes de prueba para los contribuidores los cuales se borrarán al terminar la prueba

--- ColegioHogwarts/src/FuncionesCH/DemoCH.java
+++ ColegioHogwarts/src/FuncionesCH/DemoCH.java
@@ -4,6 +4,8 @@ package FuncionesCH;
 public class DemoCH {
     public static void main(String[] args) {
         System.out.println("Bienvenido al programa del Colegio Hogwarts");
+        System.out.println("Comienza el juego");
+        System.out.println("Y ahora");
     }
     
 }
