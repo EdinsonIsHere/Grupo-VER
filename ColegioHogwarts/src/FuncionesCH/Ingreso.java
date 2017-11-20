55ce56d07d6cab4ae6bb1f1fb4a44b1ae3e58e3e
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 19:59:52 -0500

    Se añade el metodo Ingreso
    El cual pide la usuario y contraseña para ver si son válidos

--- ColegioHogwarts/src/FuncionesCH/Ingreso.java
+++ ColegioHogwarts/src/FuncionesCH/Ingreso.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FuncionesCH;
 
-/**
- *
- * @author PAQUITA
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Ingreso {
+    private String usuario;
+    private String contraseña;
+    private boolean usuarioValido;
+    private boolean contraseñaValida;
+    
+    
+    public Ingreso(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su usuario: ");
+        usuario= sc.next();
+        System.out.println("Ingrese su contraseña: ");
+        contraseña= sc.next();
+        
+        BufferedReader archivo = new BufferedReader( new FileReader(""
+                + "https://docs.google.com/document/d/1FftqXE5YHozby59kMHCTVANnmpd65txYZE2BfJMWu6A/edit?usp=sharing"));
+        
+        //Validar el usuario
+        String linea = "";
+        while((linea = archivo.readLine())!= null){
+            if(linea.indexOf(",")!=-1){
+                if (linea.split(",")[0].equalsIgnoreCase(usuario)){
+                    usuarioValido = true;
+                    break;
+                }
+            }
+
+        }
+    }
+    
+    
+    
+    
     
 }
