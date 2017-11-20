1b11b466ae15f9c351d9d47fdbed502b65c42bf9
Víctor Espinoza Molina <33600599+VictorJEM@users.noreply.github.com>
2017-11-20 10:57:56 -0500

    Clase Persona
    Se creo la clase Persona.

---
+++ ColegioHogwarts/src/FuncionesCH/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FuncionesCH;
+
+/**
+ *
+ * @author CLARO
+ */
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String varita;
+    private Hechizero tipoMago;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getVarita() {
+        return varita;
+    }
+
+    public void setVarita(String varita) {
+        this.varita = varita;
+    }
+
+    public Hechizero getTipoMago() {
+        return tipoMago;
+    }
+
+    public void setTipoMago(Hechizero tipoMago) {
+        this.tipoMago = tipoMago;
+    }
+}
