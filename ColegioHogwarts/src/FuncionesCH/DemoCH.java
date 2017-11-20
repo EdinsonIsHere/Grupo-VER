1c779f8525e6d1b4dd026c6e003a5eb2cca935e6
RogwiCajas <rogwinalex2@hotmail.com>
2017-11-19 21:08:28 -0500

    prubea de github2
    prueba de github 2

--- ColegioHogwarts/src/FuncionesCH/DemoCH.java
+++ ColegioHogwarts/src/FuncionesCH/DemoCH.java
@@ -4,6 +4,7 @@ package FuncionesCH;
 public class DemoCH {
     public static void main(String[] args) {
         System.out.println("Bienvenido al programa del Colegio Hogwarts");
+        System.out.println("Just testing");
         
     }
     
