ff792a05785132ba39961d0d9d24496282cccf18
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 23:12:14 -0500

    Ingreso con archivo local
    Ingreso del usuario con contraseña de un archivo local terminado.
    Se añadió excepciones porque así lo sugeria netbeans.

--- ColegioHogwarts/src/FuncionesCH/Ingreso.java
+++ ColegioHogwarts/src/FuncionesCH/Ingreso.java
@@ -1,50 +1,42 @@
 package FuncionesCH;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.net.URI;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Ingreso {
-    private String usuario;
-    private String contraseña;
+    private String linea = null;
+    private final String usuario;
+    private final String contraseña;
     private boolean acceso= false;
    
     
-    public Ingreso(){
+    public Ingreso() throws FileNotFoundException, IOException{
+ 
+        FileReader archivo = new FileReader("C:\\Users\\PAQUITA\\Documents\\2017-II\\POO\\Proyecto\\Grupo-VER\\datos.txt");
+        BufferedReader br = new BufferedReader(archivo);
+        
         System.out.println("Edinson está en esto ahora XD");
-        /*
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese su usuario: ");
         usuario= sc.nextLine();
         System.out.println("Ingrese su contraseña: ");
         contraseña= sc.nextLine();
         
-        FileReader archivo = new FileReader("datos.txt");
-        
-        //Validar el usuario
-        String linea = "";
-        while((linea = archivo.readLine())!= null){
-            if(linea.indexOf(",")!=-1){
-                if (linea.split(",")[0].equalsIgnoreCase(usuario) && linea.split(",")[1].equalsIgnoreCase(contraseña)){
-                    acceso= true
-                    break;
+        while ((linea = br.readLine()) != null){
+            if (linea.split(",")[0].equalsIgnoreCase(usuario)){
+                if (linea.split(",")[1].equalsIgnoreCase(contraseña)){
+                    acceso = true;
                 }
             }
         }
+
         if (acceso== true){
             System.out.println("Puede entrar");
-        else{
+        }else{
             System.out.println(":O ");
-            }
         }
-    */
-    
     }
-    
-    
-    
-    
-    
 }
