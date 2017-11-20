8a5a0abb5067e96d029589aafb7bb5ffad4cad42
Víctor Espinoza Molina <33600599+VictorJEM@users.noreply.github.com>
2017-11-20 11:03:55 -0500

    Clase Profesor
    Creación de la clase Profesor

---
+++ ColegioHogwarts/src/FuncionesCH/Profesor.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FuncionesCH;
+
+/**
+ *
+ * @author CLARO
+ */
+public class Profesor extends Persona{
+    private String fechaIngreso;
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+}
