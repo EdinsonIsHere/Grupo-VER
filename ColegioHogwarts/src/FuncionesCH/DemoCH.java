6c067b9a944b15eaa33a9f3a9372ffa3238b7e41
Víctor Espinoza Molina <33600599+VictorJEM@users.noreply.github.com>
2017-11-27 21:18:41 -0500

    Algunos métodos del main.

--- ColegioHogwarts/src/FuncionesCH/DemoCH.java
+++ ColegioHogwarts/src/FuncionesCH/DemoCH.java
@@ -12,4 +12,21 @@ public class DemoCH {
         System.out.println("El tipo de persona es : "+ tipoPersona);
         
     }
+    
+    public static void showMenuPlanificador(){
+        System.out.println("/** PLANIFICADOR **/\n");
+        System.out.println("1. Crear Curso");
+        System.out.println("2. Crear Profesor");
+        System.out.println("3. Crear Estudiante");
+        System.out.println("4. Ver Horarios Planificados");
+        System.out.println("5. Listado de Estudiantes");
+    }
+    
+    public static void showMenuEstudiantes(){
+        System.out.println("/** ESTUDIANTE **/\n");
+        System.out.println("1. Ver Cursos Planificados");
+        System.out.println("2. Registro");
+        System.out.println("3. Descripción de Vuelo");
+    }
+    
 }
