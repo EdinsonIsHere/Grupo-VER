0794efc01b83c860cdfcd246aab611c01d6c48a9
EdinsonIsHere <sir.sored@gmail.com>
2017-11-28 22:09:00 -0500

    Revert "correción clase profesor"
    This reverts commit cbdbdab6a28e07bdbaf61c23154d72a475bcac6f.

--- ColegioHogwarts/src/FuncionesCH/Academico_Hogwarts.java
+++ ColegioHogwarts/src/FuncionesCH/Academico_Hogwarts.java
@@ -1,7 +1,15 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package FuncionesCH;
 import java.util.ArrayList;
 
+/**
+ *
+ * @author CLARO
+ */
 public class Academico_Hogwarts {
     
     private ArrayList<Estudiante> estudiantes;
